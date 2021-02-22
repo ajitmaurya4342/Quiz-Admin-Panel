66df256c097cfd0cc5e8164aec9e1147be381b23
ajit3216 <ajit@binarynumbers.io>
2021-02-23 00:38:58 +0530

    Updates

---
+++ precache-manifest.e70e77e702aae14b7be17afe38043069.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "33e4dbd785138def6c22",
+    "url": "/react-reduction/static/js/6.8791c04c.chunk.js"
+  },
+  {
+    "revision": "0cf7eb3928d15ff29265",
+    "url": "/react-reduction/static/js/0.d2a89a21.chunk.js"
+  },
+  {
+    "revision": "ecf158e15f3fc53d8636",
+    "url": "/react-reduction/static/js/2.af2ffef1.chunk.js"
+  },
+  {
+    "revision": "159100485eb51191d396",
+    "url": "/react-reduction/static/js/12.5e484201.chunk.js"
+  },
+  {
+    "revision": "09e0142c68f8280c25fa",
+    "url": "/react-reduction/static/js/main.46b4fdfd.chunk.js"
+  },
+  {
+    "revision": "aa8baaa7ad75a82ccd6f",
+    "url": "/react-reduction/static/js/runtime~main.9a0e9a1f.js"
+  },
+  {
+    "revision": "2148466207b0aca41c0a",
+    "url": "/react-reduction/static/js/11.4d3f662b.chunk.js"
+  },
+  {
+    "revision": "f8d730abcd699279b1d1",
+    "url": "/react-reduction/static/js/5.c55af21c.chunk.js"
+  },
+  {
+    "revision": "0663e389e42142d28ce3",
+    "url": "/react-reduction/static/js/1.586b65e0.chunk.js"
+  },
+  {
+    "revision": "e46ef88a3bcfe8648a92",
+    "url": "/react-reduction/static/js/7.74c2a9e0.chunk.js"
+  },
+  {
+    "revision": "63b64153e04deb516d00",
+    "url": "/react-reduction/static/js/8.2d21bd12.chunk.js"
+  },
+  {
+    "revision": "c8d30161a972e9cffb07",
+    "url": "/react-reduction/static/js/9.1316f1a5.chunk.js"
+  },
+  {
+    "revision": "4014b1755a13b3405bbd",
+    "url": "/react-reduction/static/js/10.525d66a8.chunk.js"
+  },
+  {
+    "revision": "09e0142c68f8280c25fa",
+    "url": "/react-reduction/static/css/main.5811d0ea.chunk.css"
+  },
+  {
+    "revision": "f8d730abcd699279b1d1",
+    "url": "/react-reduction/static/css/5.cdd36044.chunk.css"
+  },
+  {
+    "revision": "9c350b66c31694070b371623fcc288cb",
+    "url": "/react-reduction/index.html"
+  }
+];
